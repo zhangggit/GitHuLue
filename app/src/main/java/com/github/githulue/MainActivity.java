5fcda1027943bb8d9457a00d6845ddaeb542383d
zhanggang <zhang834826337@qq.com>
2017-10-19 15:12:22 +0800

    启动优化解决白屏

---
+++ app/src/main/java/com/github/githulue/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.githulue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_main);
+    }
+}
